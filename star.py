1fb215fe5c4026c680115656626a8abbd051e30c
Jack <FurbyJL@cardiff.ac.uk>
2018-10-29 21:48:40 +0000

    started to implement trie data structure

--- star.py
+++ star.py
@@ -1,17 +1,46 @@
 """STAR."""
-import sys
+import glob
+import errno
+import csv
 
 
 class Node:
 	"""Node object for binary search tree."""
 
 	def __init__(self, key):
-		"""Each node has a value, indicator, lower node pointer, higher node pointer and middle node pointer."""
-		self.lower = None
-		self.higher = None
-		self.middle = None
-		self.indicator = 0
-		self.val = key
+		"""Each node has a value, indicator, and 26 node pointers (one for every letter in alphabet)."""
+		"Each of the pointers will point to another node or None"
+		"If indicator == True then all of the nodes leading up to and including the current node makes an accepted word"
+		"value is used to hold the current concatination of letters"
+		self.a = None
+		self.a = None
+		self.b = None
+		self.c = None
+		self.d = None
+		self.e = None
+		self.f = None
+		self.g = None
+		self.h = None
+		self.i = None
+		self.j = None
+		self.k = None
+		self.l = None
+		self.m = None
+		self.n = None
+		self.o = None
+		self.p = None
+		self.q = None
+		self.r = None
+		self.s = None
+		self.t = None
+		self.u = None
+		self.v = None
+		self.w = None
+		self.x = None
+		self.y = None
+		self.z = None
+		self.indicator = False
+		self.value = None
 
 
 def insert():
@@ -20,20 +49,22 @@ def insert():
 
 def setup():
 	"""Create data structure of words and points."""
+	path = './words/*.csv'
+	files = glob.glob(path)
+	for name in files:
+		try:
+			with open(name) as csvfile:
+				reader = csv.reader(csvfile, delimiter=',')
+				for row in reader:
+					print(row[0])
+		except IOError as exc:
+			if exc.errno != errno.EISDIR:
+				raise
 
 
-if len(sys.argv) > 1:
-	input1 = sys.argv[1]
-	if input1 == '-r':
-		if len(sys.argv) > 2:
-			# run program for given characters
-			pass
-		else:
-			print('Not enough arguments')
-	elif input1 == '-help':
-		print('==== help entered ====')
-		print('-r [x] run program. If true is returned then input is accepted')
-	else:
-		print('Input not recognised')
-else:
-	print('Input not recognised')
+def main():
+	setup()
+
+
+if __name__ == '__main__':
+	main()
