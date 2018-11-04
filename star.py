97b967b462886f698844aae3bacfb14916cd8ee7
Jack <FurbyJL@cardiff.ac.uk>
2018-11-04 00:09:14 +0000

    Add and search for a match in trie

--- star.py
+++ star.py
@@ -5,50 +5,66 @@ import csv
 
 
 class Node:
-	"""Node object for binary search tree."""
+	"""Node object for trie."""
 
-	def __init__(self, key):
-		"""Each node has a value, indicator, and 26 node pointers (one for every letter in alphabet)."""
-		"Each of the pointers will point to another node or None"
-		"If indicator == True then all of the nodes leading up to and including the current node makes an accepted word"
-		"value is used to hold the current concatination of letters"
-		self.a = None
-		self.a = None
-		self.b = None
-		self.c = None
-		self.d = None
-		self.e = None
-		self.f = None
-		self.g = None
-		self.h = None
-		self.i = None
-		self.j = None
-		self.k = None
-		self.l = None
-		self.m = None
-		self.n = None
-		self.o = None
-		self.p = None
-		self.q = None
-		self.r = None
-		self.s = None
-		self.t = None
-		self.u = None
-		self.v = None
-		self.w = None
-		self.x = None
-		self.y = None
-		self.z = None
-		self.indicator = False
-		self.value = None
+	# children contains linkks to other nodes
+	# data contains current letter
+	# end indicates if the current node is the end of a word
+	def __init__(self, end=False, data=None):
+		"""Initilise the node."""
+		self.end = end
+		self.data = data
+		self.children = dict()
 
 
-def insert():
-	"""Insert a word into the data structure."""
+class Trie:
+	"""Trie data structure."""
+
+	def __init__(self):
+		"""Initilise the trie."""
+		self.head = Node()
+
+	def addWord(self, word):
+		"""Add a word to the trie."""
+		currentNode = self.head
+
+		for i in range(len(word)):
+			# print(word)
+			# print(word[i])
+			# print(word[i] in currentNode.children)
+			# print(currentNode.children)
+			if word[i] in currentNode.children:
+				currentNode = currentNode.children[word[i]]
+				if i == len(word) - 1:
+					currentNode.end = True
+			else:
+				if i < len(word) - 1:
+					currentNode.children[word[i]] = Node(False)
+				else:
+					currentNode.children[word[i]] = Node(True, word)
+				currentNode = currentNode.children[word[i]]
+
+	def hasWord(self, word):
+		"""If the trie has the word being searched for return true."""
+		if word is '' or word is None:
+			return False
+
+		currentNode = self.head
+		for letter in word:
+			# print(letter)
+			# print(letter in currentNode.children)
+			# print(currentNode.end)
+			if letter in currentNode.children:
+				currentNode = currentNode.children[letter]
+
+		if currentNode.end is False:
+			return False
+		else:
+			return True
 
 
-def setup():
-	"""Create data structure of words and points."""
+def setup(trie):
+	"""Add each word from csv files to trie data structure."""
 	path = './words/*.csv'
 	files = glob.glob(path)
 	for name in files:
@@ -56,15 +72,22 @@ def setup():
 			with open(name) as csvfile:
 				reader = csv.reader(csvfile, delimiter=',')
 				for row in reader:
-					print(row[0])
+					trie.addWord(row[0].lower())
 		except IOError as exc:
 			if exc.errno != errno.EISDIR:
 				raise
 
 
-def main():
-	setup()
-
-
 if __name__ == '__main__':
-	main()
+	""" Example use """
+	trie = Trie()
+	setup(trie)
+	# words = 'ice iceberg icebergs iceblink iceblinks icebox iceboxes iced'
+	# for word in words.split():
+	# 	trie.addWord(word)
+	print("good in trie:", trie.hasWord('good'))
+	print("goodbye in trie:", trie.hasWord('goodbye'))
+	print("sumo in trie:", trie.hasWord('sumo'))
+	print("iced in trie:", trie.hasWord('iced'))
+	print("icebox in trie:", trie.hasWord('icebox'))
+	print("iceboxes in trie:", trie.hasWord('iceboxes'))
