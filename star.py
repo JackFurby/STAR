17a1c674a9af4f4e41f6455de74f50cc1add4dc1
Jack <furbyjl@cardiff.ac.uk>
2018-12-02 18:59:29 +0000

    Added menu and access to hasWord

--- star.py
+++ star.py
@@ -16,21 +16,43 @@ if __name__ == '__main__':
 	run = True
 
 	while run:
-		inputLetters = input("Enter letters ('?' is a wildcard): ")
+		action = input("What do you want to do? Enter 'help' for more: ")
 
-		if inputLetters == '\q':
+		if action == "\q":
 			run = False
 			break
+		elif action == "isAccepted":
+			inputLetters = input("Enter a word to check: ")
 
-		# Makes sure input is in lower case
-		inputLetters = inputLetters.lower()
+			# Makes sure input is in lower case
+			inputLetters = inputLetters.lower()
 
-		start = time.time()
-		wordList = trie.wordSearch(list(inputLetters))
-		wordList.sort(key=lambda tup: -tup[1])
-		print(*wordList, sep='\n')
-		end = time.time()
-		print("Completed search in", end - start, 'seconds')
+			if trie.hasWord(inputLetters):
+				print("Yes")
+			else:
+				print("No")
+		elif action == "findWords":
+			inputLetters = input("Enter letters ('?' is a wildcard): ")
+
+			# Makes sure input is in lower case
+			inputLetters = inputLetters.lower()
+
+			start = time.time()
+			wordList = trie.wordSearch(list(inputLetters))
+			wordList.sort(key=lambda tup: -tup[1])
+			print(*wordList, sep='\n')
+			end = time.time()
+			print("Completed search in", end - start, 'seconds')
+		elif action == "help":
+			print("")
+			print("=== STAR help ===")
+			print("")
+			print("\q		-	Exit STAR")
+			print("isAccepted	-	Enter a single word to find out if it is accepted or not")
+			print("findWords	-	Find all words you can make with a given set of characters")
+			print("")
+		else:
+			print("Input not recognised")
 
 	# print(board)
 	# print("goodbye in trie:", trie.hasWord('goodbye'))
